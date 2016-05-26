8c150478fdfaff8c35723f9166eb5ac69c4853eb
Ian Wootten <i.wootten@gmail.com>
2016-05-26 21:05:27 +0100

    Correct derivation errors

--- pcktransformer.py
+++ pcktransformer.py
@@ -10,21 +10,21 @@ form_ids = {
 
 def get_form_questions(survey):
     questions = []
-    question_types = []
+    question_types = {}
 
     for answer_group in survey['answer_groups']:
         for answer in answer_group['answers']:
-            questions.append(int(answer['question_id']))
-            if answer['type']:
-                question_types.append(answer['type'])
+            question_id = answer['question_id']
+            questions.append(int(question_id))
+            if 'type' in answer:
+                question_types[question_id] = answer['type']
 
-        return questions, question_types
+    return questions, question_types
 
 
 def get_derived_value(form_question_types, question_id, value):
     if question_id in form_question_types:
         form_question_type = form_question_types[question_id]
-
         if form_question_type == 'contains':
             value = "1" if value else "2"
         elif form_question_type == 'date':
@@ -36,12 +36,12 @@ def get_derived_value(form_question_types, question_id, value):
 
 
 def derive_answers(survey, answers):
-    answers = []
+    derived = []
 
     form_questions, form_question_types = get_form_questions(survey)
 
     for k, v in answers:
         if int(k) in form_questions:
-            answers.append((int(k), get_derived_value(form_question_types, k, v)))
+            derived.append((int(k), get_derived_value(form_question_types, k, v)))
 
-    return sorted(answers)
+    return sorted(derived)
