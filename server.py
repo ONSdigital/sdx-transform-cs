b2a51d809089a6d980db4b8333ab7b31294f7a67
iwootten <i.wootten@gmail.com>
2016-06-09 18:22:05 +0100

    Add styling changes

--- server.py
+++ server.py
@@ -65,6 +65,51 @@ def html():
         return template.render(response=response, survey=survey)
 
 
+@app.route('/html-test', methods=['GET'])
+def html_test():
+    message = '''{
+       "type": "uk.gov.ons.edc.eq:surveyresponse",
+       "origin": "uk.gov.ons.edc.eq",
+       "survey_id": "023",
+       "version": "0.0.1",
+       "collection": {
+         "exercise_sid": "hfjdskf",
+         "instrument_id": "0203",
+         "period": "0216"
+       },
+       "submitted_at": "2016-03-12T10:39:40Z",
+       "metadata": {
+         "user_id": "789473423",
+         "ru_ref": "12345678901A"
+       },
+       "data": {
+         "11": "01/04/2016",
+         "12": "31/10/2016",
+         "20": "1800000",
+         "51": "84",
+         "52": "10",
+         "53": "73",
+         "54": "24",
+         "50": "205",
+         "22": "705000",
+         "23": "900",
+         "24": "74",
+         "25": "50",
+         "26": "100",
+         "21": "60000",
+         "27": "7400",
+         "146": "some comment"
+       }
+    }'''
+
+    response = json.loads(message)
+    template = env.get_template('html.tmpl')
+    form_id = response['collection']['instrument_id']
+
+    with open("./surveys/%s.%s.json" % (response['survey_id'], form_id)) as json_file:
+        survey = json.load(json_file)
+        return template.render(response=response, survey=survey)
+
 if __name__ == '__main__':
     # Startup
     logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
