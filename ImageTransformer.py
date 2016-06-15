330b71cc9e281fc653a6e3090db0bf1253efce7d
Ian Wootten <i.wootten@gmail.com>
2016-06-15 20:42:14 +0100

    Move jinja filters into separate file

--- ImageTransformer.py
+++ ImageTransformer.py
@@ -6,54 +6,18 @@ import datetime
 import dateutil.parser
 import re
 import settings
-from jinja2 import Environment, PackageLoader
 from PDFTransformer import PDFTransformer
-
-env = Environment(loader=PackageLoader('transform', 'templates'))
-
-def format_date(value, style='long'):
-    """convert a datetime to a different format."""
-
-    date_format = '%Y%m%d' if style == 'short' else '%d/%m/%Y %H:%M:%S'
-    return value.strftime(date_format)
-
-def statistical_unit_id_filter(value):
-    if len(value) == 12:
-        return value[0:-1]
-
-def scan_id_filter(value):
-    scanfile, _ = os.path.splitext(value)
-
-    return scanfile
-
-def page_filter(value):
-    page = str(value).zfill(3) if value else ''
-
-    return "%s,0" % page if value == 1 else page
-
-def format_period(value):
-    if not value:
-        return ''
-
-    if len(value) == 4:
-        return "20%s" % value
-    elif len(value) < 6:
-        return value.zfill(6)
-    elif len(value) > 6:
-        return value[0:6]
-
-env.filters['format_date'] = format_date
-env.filters['statistical_unit_id'] = statistical_unit_id_filter
-env.filters['scan_id'] = scan_id_filter
-env.filters['format_page'] = page_filter
-env.filters['format_period'] = format_period
+from image_filters import get_env, format_date
 
 class ImageTransformer(object):
     def __init__(self, survey, response_data):
         self.survey = survey
         self.response = response_data
 
-        # We need to generate a pdf first
+    def create_pdf(self):
+        '''
+        Create a pdf which will be used as the basis for images
+        '''
         pdf_transformer = PDFTransformer(self.survey, self.response)
 
         self.pdf_file = pdf_transformer.render_to_file()
@@ -91,6 +55,7 @@ class ImageTransformer(object):
         '''
         Takes a list of images and creates a index csv from them
         '''
+        env = get_env()
         template = env.get_template('csv.tmpl')
 
         current_time = datetime.datetime.now()
@@ -99,21 +64,24 @@ class ImageTransformer(object):
 
         template_output = template.render(IMAGE_PATH=settings.IMAGE_PATH, images=self.images, response=self.response, creation_time=current_time)
 
-        index_file = "EDC_%s_%s_%04d.csv" %(self.survey['survey_id'], submission_date_str, sequence_no)
+        self.index_file = "EDC_%s_%s_%04d.csv" %(self.survey['survey_id'], submission_date_str, sequence_no)
 
-        with open(index_file, "w") as fh:
+        with open(self.index_file, "w") as fh:
             fh.write(template_output)
 
-        return template_output
-
-    def create_image_zip(self):
+    def create_zip(self):
         '''
         Create a zip from a renumbered sequence
         '''
-        zipf = zipfile.ZipFile('%s.zip' % self.rootname, 'w', zipfile.ZIP_DEFLATED)
+        print("PATH: %s" % self.path)
+        zipname = '%s/%s.zip' % (self.path , self.rootname)
+        zipf = zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED)
 
-        for file in self.create_image_sequence():
-            zipf.write(os.path.join(self.path, file))
+        for file in self.images:
+            zipf.write(file)
+
+        if self.index_file:
+            zipf.write(self.index_file)
 
         zipf.close()
 
@@ -127,3 +95,6 @@ class ImageTransformer(object):
 
         for image in self.images:
             os.remove(os.path.join(self.path, image))
+
+        if self.index_file:
+            os.remove(os.path.join(self.path, self.index_file))
---
+++ image_filters.py
@@ -0,0 +1,44 @@
+import os
+from jinja2 import Environment, PackageLoader
+
+def format_date(value, style='long'):
+    """convert a datetime to a different format."""
+
+    date_format = '%Y%m%d' if style == 'short' else '%d/%m/%Y %H:%M:%S'
+    return value.strftime(date_format)
+
+def statistical_unit_id_filter(value):
+    if len(value) == 12:
+        return value[0:-1]
+
+def scan_id_filter(value):
+    scanfile, _ = os.path.splitext(value)
+
+    return scanfile
+
+def page_filter(value):
+    page = str(value).zfill(3) if value else ''
+
+    return "%s,0" % page if value == 1 else page
+
+def format_period(value):
+    if not value:
+        return ''
+
+    if len(value) == 4:
+        return "20%s" % value
+    elif len(value) < 6:
+        return value.zfill(6)
+    elif len(value) > 6:
+        return value[0:6]
+
+def get_env():
+    env = Environment(loader=PackageLoader('transform', 'templates'))
+
+    env.filters['format_date'] = format_date
+    env.filters['statistical_unit_id'] = statistical_unit_id_filter
+    env.filters['scan_id'] = scan_id_filter
+    env.filters['format_page'] = page_filter
+    env.filters['format_period'] = format_period
+
+    return env
