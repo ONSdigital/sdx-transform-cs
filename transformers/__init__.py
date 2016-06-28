1209ffcb0d72538b948fc7b0ff29c383d16e2469
Ian Wootten <i.wootten@gmail.com>
2016-06-28 13:59:12 +0100

    Refactor the PCKTransformer so it operates as a class

--- transformers/__init__.py
+++ transformers/__init__.py
@@ -1,6 +1,6 @@
 from .CSTransformer import CSTransformer
 from .PDFTransformer import PDFTransformer
 from .ImageTransformer import ImageTransformer
-from .pcktransformer import derive_answers, form_ids
+from .PCKTransformer import PCKTransformer
 
-__all__ = ['CSTransformer', 'PDFTransformer', 'ImageTransformer', 'derive_answers', 'form_ids']
+__all__ = ['CSTransformer', 'PDFTransformer', 'ImageTransformer', 'PCKTransformer']
