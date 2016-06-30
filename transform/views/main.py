fd89b17952c6b7e2126151c8799e723cd33515b0
iwootten <i.wootten@gmail.com>
2016-06-30 16:32:40 +0100

    Add logging/exceptions

--- transform/views/main.py
+++ transform/views/main.py
@@ -1,20 +1,49 @@
 from transform import app
 
-from flask import request, make_response, send_file
+from flask import request, make_response, send_file, jsonify
 from transformers import PCKTransformer, PDFTransformer, ImageTransformer, CSTransformer
 from jinja2 import Environment, PackageLoader
 
-import os
 import json
 
 env = Environment(loader=PackageLoader('transform', 'templates'))
 
 
-def get_survey(survey_response):
-    form_id = survey_response['collection']['instrument_id']
+@app.errorhandler(400)
+def known_error(error=None):
+    app.logger.error("sdx-transform-cs:TRANSFORM:FAILURE:%s", repr(error))
+    message = {
+        'status': 400,
+        'message': repr(error),
+        'uri': request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 400
 
-    with open("./surveys/%s.%s.json" % (survey_response['survey_id'], form_id)) as json_file:
-        return json.load(json_file)
+    return resp
+
+
+@app.errorhandler(500)
+def unknown_error(error=None):
+    app.logger.error("sdx-transform-cs:TRANSFORM:FAILURE:%s", repr(error))
+    message = {
+        'status': 500,
+        'message': "Internal server error: " + repr(error),
+    }
+    resp = jsonify(message)
+    resp.status_code = 500
+
+    return resp
+
+
+def get_survey(survey_response):
+    try:
+        form_id = survey_response['collection']['instrument_id']
+
+        with open("./surveys/%s.%s.json" % (survey_response['survey_id'], form_id)) as json_file:
+            return json.load(json_file)
+    except IOError:
+        return False
 
 
 @app.route('/pck', methods=['POST'])
@@ -23,6 +52,9 @@ def render_pck(batch_number=False):
     response = request.get_json(force=True)
     template = env.get_template('pck.tmpl')
     survey = get_survey(response)
+
+    if not survey:
+        return known_error("Unsupported survey/instrument id")
 
     if batch_number:
         batch_number = int(batch_number)
@@ -52,6 +84,9 @@ def render_html():
 
     survey = get_survey(response)
 
+    if not survey:
+        return known_error("Unsupported survey/instrument id")
+
     return template.render(response=response, survey=survey)
 
 
@@ -61,8 +96,15 @@ def render_pdf():
 
     survey = get_survey(survey_response)
 
-    pdf = PDFTransformer(survey, survey_response)
-    rendered_pdf = pdf.render()
+    if not survey:
+        return known_error("Unsupported survey/instrument id")
+
+    try:
+        pdf = PDFTransformer(survey, survey_response)
+        rendered_pdf = pdf.render()
+
+    except IOError as e:
+        return known_error("Could not render pdf buffer: %s" % repr(e))
 
     response = make_response(rendered_pdf)
     response.mimetype = 'application/pdf'
@@ -76,14 +118,19 @@ def render_images():
 
     survey = get_survey(survey_response)
 
+    if not survey:
+        return known_error("Unsupported survey/instrument id")
+
     itransformer = ImageTransformer(survey, survey_response)
 
-    itransformer.create_pdf()
-    itransformer.create_image_sequence()
-    itransformer.create_image_index()
-    zipname = itransformer.create_zip()
-    zipfile = os.path.join(itransformer.path, zipname)
-    itransformer.cleanup()
+    try:
+        itransformer.create_pdf()
+        itransformer.create_image_sequence()
+        itransformer.create_image_index()
+        zipfile = itransformer.create_zip()
+        itransformer.cleanup()
+    except IOError as e:
+        return known_error("Could not create zip buffer: %s" % repr(e))
 
     return send_file(zipfile, mimetype='application/zip')
 
@@ -101,6 +148,9 @@ def common_software(sequence_no=1000, batch_number=False):
         sequence_no = int(sequence_no)
 
     survey = get_survey(survey_response)
+
+    if not survey:
+        return known_error("Unsupported survey/instrument id")
 
     ctransformer = CSTransformer(survey, survey_response, batch_number, sequence_no)
 
