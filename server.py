223abcdda486fcc8a77e191792047a9712eb666e
iwootten <i.wootten@gmail.com>
2016-07-04 14:17:38 +0100

    Use a handler that actually writes to file :)

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 import settings
+from settings import MakeFileHandler
 from transform import app
 import logging
-import logging.handlers
+from logging import Formatter
+
 
 if __name__ == '__main__':
     # Startup
     logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
+    handler = MakeFileHandler(settings.LOGGING_LOCATION, maxBytes=20000, backupCount=5)
+    handler.setFormatter(Formatter(settings.LOGGING_FORMAT))
+    app.logger.addHandler(handler)
     app.run(debug=True, host='0.0.0.0')
