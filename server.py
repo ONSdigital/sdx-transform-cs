f4a0d736f2df0a622b85a8655cfc37a2030890ca
iwootten <i.wootten@gmail.com>
2016-07-05 10:49:49 +0100

    Revert "Set logger for werkzeug server"
    This reverts commit 5eaa15d36897b4aae2cf265213de80e2dd07d43d.

--- server.py
+++ server.py
@@ -9,7 +9,5 @@ if __name__ == '__main__':
     logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
     handler = logging.handlers.RotatingFileHandler(settings.LOGGING_LOCATION, maxBytes=20000, backupCount=5)
     handler.setFormatter(logging.Formatter(settings.LOGGING_FORMAT))
-    log = logging.getLogger('werkzeug')
-    log.addHandler(handler)
     app.logger.addHandler(handler)
     app.run(debug=True, host='0.0.0.0')
