655f616f00ff240154257b81f06c1da69349a866
Jenn <jennifer.edwards@methods.co.uk>
2016-07-14 15:08:46 +0100

    flake8 tidy-up

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import settings
 from transform import app
 import logging
 import logging.handlers
-import os
 
 
 if __name__ == '__main__':
@@ -10,4 +9,3 @@ if __name__ == '__main__':
     logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
     handler = logging.handlers.RotatingFileHandler(settings.LOGGING_LOCATION, maxBytes=20000, backupCount=5)
     app.run(debug=True, host='0.0.0.0')
-
