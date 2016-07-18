c971f158a53ffab5608aa9dd7db0e4aa14001fc2
iwootten <i.wootten@gmail.com>
2016-07-18 14:20:00 +0100

    Put port removal back removed in error

--- server.py
+++ server.py
@@ -2,10 +2,11 @@ import settings
 from transform import app
 import logging
 import logging.handlers
+import os
 
 
 if __name__ == '__main__':
     # Startup
     logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
-    handler = logging.handlers.RotatingFileHandler(settings.LOGGING_LOCATION, maxBytes=20000, backupCount=5)
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.getenv("PORT"))
+    app.run(debug=True, host='0.0.0.0', port=port)
