7f3d389b08319a4e733228f0fe021404cce9d335
tundish <tundish@thuswise.org>
2017-02-07 09:53:47 +0000

    Added format for form_header.

--- tests/test_mwss_transform.py
+++ tests/test_mwss_transform.py
@@ -5,8 +5,16 @@ from transform.transformers.MWSSTransformer import CSFormatter
 
 class BatchFileTests(unittest.TestCase):
 
-    def test_header(self):
+    def test_batch_header(self):
         batchNo = 3866
         batchDate = datetime.date(2009, 12, 29)
-        rv = CSFormatter.header(batchNo, batchDate)
+        rv = CSFormatter.batch_header(batchNo, batchDate)
         self.assertEqual("FBFV00386629/12/09", rv)
+
+    def test_form_header(self):
+        formId = 4
+        ruRef = 49900001225
+        check = "C"
+        period = "200911"
+        rv = CSFormatter.form_header(formId, ruRef, check, period)
+        self.assertEqual("0004:49900001225C:200911", rv)
--- transform/transformers/MWSSTransformer.py
+++ transform/transformers/MWSSTransformer.py
@@ -2,6 +2,17 @@
 class CSFormatter:
 
     @staticmethod
-    def header(batchNo, ts):
+    def batch_header(batchNo, ts):
         return "{0}{1:06}{2}".format("FBFV", batchNo, ts.strftime("%d/%m/%y"))
 
+    @staticmethod
+    def form_header(formId, ruRef, check, period):
+        formId = "{0:04}".format(formId) if isinstance(formId, int) else formId
+        return "{0}:{1}{2}:{3}".format(formId, ruRef, check, period)
+
+    @staticmethod
+    def pck_lines(batchNo, ts, data):
+        return [
+            header(batchNo, ts),
+            "FV",
+        ]
