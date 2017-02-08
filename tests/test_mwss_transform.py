8d2d222fb61b2a33f5088e6befd852b390d41992
tundish <tundish@thuswise.org>
2017-02-08 11:22:42 +0000

    Added binding for logger.

--- tests/test_mwss_transform.py
+++ tests/test_mwss_transform.py
@@ -4,6 +4,8 @@ import json
 import unittest
 
 from transform.transformers.MWSSTransformer import CSFormatter
+from transform.transformers.MWSSTransformer import MWSSTransformer
+from transform.transformers.MWSSTransformer import Survey
 
 import pkg_resources
 
@@ -51,14 +53,15 @@ class BatchFileTests(unittest.TestCase):
         reply = pkg_resources.resource_string(__name__, "pck/023.0102.json")
         data = json.loads(reply.decode("utf-8"))
         data["tx_id"] = "27923934-62de-475c-bc01-433c09fd38b8"
-        ids = CSFormatter.identifiers(data)
-        self.assertIsInstance(ids, CSFormatter.Identifiers)
+        ids = Survey.identifiers(data)
+        self.assertIsInstance(ids, Survey.Identifiers)
         self.assertEqual(0, ids.batchNr)
         self.assertEqual(0, ids.seqNr)
         self.assertEqual(data["tx_id"], ids.txId)
         self.assertEqual(datetime.date.today(), ids.ts)
         self.assertEqual("023", ids.surveyId)
         self.assertEqual("RSI5B", ids.formId)
+        self.assertEqual("789473423", ids.userId)
         self.assertEqual("12345678901", ids.ruRef)
         self.assertEqual("A", ids.ruChk)
         self.assertEqual("1604", ids.period)
@@ -72,7 +75,7 @@ class BatchFileTests(unittest.TestCase):
             ("0140", "{0:011}".format(124)),
             ("0151", "{0:011}".format(217222))
         ])
-        ids = CSFormatter.identifiers(reply)
+        ids = Survey.identifiers(reply)
         rv = CSFormatter.pck_lines(reply["data"], **ids._asdict())
         self.assertEqual([
             "FBFV00386629/12/09",
@@ -82,3 +85,12 @@ class BatchFileTests(unittest.TestCase):
             "0140 00000000124",
             "0151 00000217222",
         ], rv)
+
+
+class PackingTests(unittest.TestCase):
+
+    def test_tempdir(self):
+        tfr = MWSSTransformer({})
+        self.assertIsNone(tfr.ids)
+        tfr.pack()
+
--- transform/transformers/MWSSTransformer.py
+++ transform/transformers/MWSSTransformer.py
@@ -1,6 +1,36 @@
 from collections import namedtuple
 import datetime
 import logging
+import tempfile
+
+
+class Survey:
+
+    Identifiers = namedtuple("Identifiers", [
+        "batchNr", "seqNr", "ts", "txId", "surveyId", "instId",
+        "formId", "userId", "ruRef", "ruChk", "period"
+    ])
+
+    @staticmethod
+    def identifiers(data, batchNr=0, seqNr=0, log=None):
+        log = log or logging.getLogger(__name__)
+        ruRef = data.get("metadata", {}).get("ru_ref", "")
+        rv = Survey.Identifiers(
+            batchNr, seqNr, datetime.date.today(),
+            data.get("tx_id"),
+            data.get("survey_id"),
+            data.get("collection", {}).get("instrument_id"),
+            CSFormatter.formIds.get(data.get("collection", {}).get("instrument_id")),
+            data.get("metadata", {}).get("user_id"),
+            ''.join(i for i in ruRef if i.isdigit()),
+            ruRef[-1] if ruRef and ruRef[-1].isalpha() else "",
+            data.get("collection", {}).get("period")
+        )
+        if any(i is None for i in rv):
+            log.warning("Missing an id from {0}".format(rv))
+            return None
+        else:
+            return rv
 
 
 class CSFormatter:
@@ -13,32 +43,6 @@ class CSFormatter:
         "141": "HE2015",
         "0102": "RSI5B",  # TODO: Check which mapping
     }
-
-    Identifiers = namedtuple("Identifiers", [
-        "batchNr", "seqNr", "ts", "txId", "surveyId", "instId",
-        "formId", "ruRef", "ruChk", "period"
-    ])
-
-    @staticmethod
-    def identifiers(data, batchNr=0, seqNr=0, log=None):
-        log = log or logging.getLogger(__name__)
-        ruRef = data.get("metadata", {}).get("ru_ref")
-        rv = CSFormatter.Identifiers(
-            batchNr, seqNr, datetime.date.today(),
-            data.get("tx_id"),
-            data.get("survey_id"),
-            data.get("collection", {}).get("instrument_id"),
-            CSFormatter.formIds.get(data.get("collection", {}).get("instrument_id")),
-            ''.join(i for i in ruRef if i.isdigit()),
-            ruRef[-1] if ruRef[-1].isalpha() else "",
-            data.get("collection", {}).get("period")
-        )
-        if any(i is None for i in rv):
-            log.warning("Missing an id from {0}".format(rv))
-            return None
-        else:
-            return rv
-
     @staticmethod
     def batch_header(batchNr, ts):
         return "{0}{1:06}{2}".format("FBFV", batchNr, ts.strftime("%d/%m/%y"))
@@ -57,3 +61,26 @@ class CSFormatter:
         ] + [
             " ".join((q, a)) for q, a in data.items()
         ]
+
+
+class MWSSTransformer:
+
+    def __init__(self, survey, log=None):
+        self.ids = Survey.identifiers(survey)
+        if log is None:
+            self.log = logging.getLogger(__name__)
+        else:
+            self.log = self.bind_logger(log, self.ids)
+        self.home = None
+
+    @staticmethod
+    def bind_logger(log, ids):
+        return log.bind(
+            ru_ref=ids.ruRef,
+            tx_id=ids.txId,
+            user_id=ids.userId,
+        )
+
+    def pack(self):
+        with tempfile.TemporaryDirectory(prefix="mwss_", dir="tmp") as self.home:
+            print(self.home)
