a42645550629339e696771a42d04c165e76bc13e
James Gardiner <james@jgardiner.co.uk>
2017-03-13 12:37:20 +0000

    Log version in startup event

--- server.py
+++ server.py
@@ -7,6 +7,6 @@ import os
 if __name__ == '__main__':
     # Startup
     logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
-    logging.info("Current version: {}".format(__version__))
+    logging.info("Starting server: version={}".format(__version__))
     port = int(os.getenv("PORT"))
     app.run(debug=True, host='0.0.0.0', port=port)
