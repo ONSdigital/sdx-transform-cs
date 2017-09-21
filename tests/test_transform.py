771cb28a4dff7dc5daa186bb8b536959bfce7f55
dcdarrell9 <darrells_king@hotmail.com>
2017-09-21 11:45:38 +0100

    ordered imports

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,17 +1,18 @@
-from transform import app
-
-import unittest
+import csv
 import glob
-import os
 import io
+import json
+import os
+import unittest
 import zipfile
 from datetime import datetime
+from unittest.mock import patch
+
 import dateutil
-import csv
-import json
+
+from transform import app
 from transform.transformers.ImageTransformer import ImageTransformer
 from transform.views.image_filters import format_date
-from unittest.mock import patch
 
 
 def get_file_as_string(filename):
