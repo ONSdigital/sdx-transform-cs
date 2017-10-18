1a80bbaae44d426e0dee7c92c9bd899b37bfd8d6
Adam Mann <manny160389@hotmail.com>
2017-10-18 10:37:54 +0100

    Remove a few unchanging config items

--- transform/settings.py
+++ transform/settings.py
@@ -11,8 +11,8 @@ SDX_SEQUENCE_URL = os.getenv("SDX_SEQUENCE_URL", "http://sdx-sequence:5000")
 
 FTP_HOST = os.getenv("FTP_PATH", "\\\\NP3RVWAPXX370\\SDX_preprod\\")
 SDX_FTP_IMAGE_PATH = os.getenv("SDX_FTP_IMAGES_PATH", "EDC_QImages")
-SDX_FTP_DATA_PATH = os.getenv("SDX_FTP_DATA_PATH", "EDC_QData")
-SDX_FTP_RECEIPT_PATH = os.getenv("SDX_FTP_RECEIPT_PATH", "EDC_QReceipts")
+SDX_FTP_DATA_PATH = "EDC_QData"
+SDX_FTP_RECEIPT_PATH = "EDC_QReceipts"
 
 # Configure the number of retries attempted before failing call
 session = requests.Session()
