7c478fcf264eab699bd348cb10951f4a51d175de
Jon Kent <jonkent95@gmail.com>
2020-07-28 10:44:39 +0100

    updated comment

--- transform/transformers/cord/cord_formatter.py
+++ transform/transformers/cord/cord_formatter.py
@@ -27,7 +27,12 @@ class CORDFormatter(Formatter):
 
     @staticmethod
     def get_idbr(survey_id, ru_ref, ru_check, period):
-        """Write an IDBR file."""
-        # the period requires a 12 to be prefixed (to represent the month)
+        """Write an IDBR file.
+        CORD currently only has one survey; E-commerce, an annual survey with a 4 digit period representing YYYY.
+        IDBR requires 6 digits and so a '12' is appended to the period to represent the month.
+        If CORD receives a period in 6 digit format then it can be passed to IDBR unaltered.
+        If in the future CORD requires a monthly survey with a 4 digit period (YYMM)
+        then this method will have to change."""
+
         period = period + "12" if len(period) == 4 else period
         return "{0}:{1}:{2:03}:{3}".format(ru_ref, ru_check, int(survey_id), period)
