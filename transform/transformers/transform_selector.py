9009a7a3cb5f1e3e791fa11a3403a3acd4440fad
Jon Kent <jonkent95@gmail.com>
2020-08-10 11:56:17 +0100

    transform selector

---
+++ transform/transformers/transform_selector.py
@@ -0,0 +1,29 @@
+
+from transform.transformers.common_software import MBSTransformer, MWSSTransformer, CSTransformer
+from transform.transformers.cora import UKISTransformer
+from transform.transformers.cord import Ecommerce2019Transformer, EcommerceTransformer
+
+
+def get_transformer(response, sequence_no=0):
+    survey_id = response['survey_id']
+
+    # CORA
+    if survey_id == "144":
+        transformer = UKISTransformer(response, sequence_no)
+
+    # CORD
+    elif survey_id == "187":
+        if response['collection']['instrument_id'] in ['0001', '0002']:
+            transformer = Ecommerce2019Transformer(response, sequence_no)
+        else:
+            transformer = EcommerceTransformer(response, sequence_no)
+
+    # COMMON SOFTWARE
+    elif survey_id == "009":
+        transformer = MBSTransformer(response, sequence_no)
+    elif survey_id == "134":
+        transformer = MWSSTransformer(response, sequence_no)
+    else:
+        transformer = CSTransformer(response, sequence_no)
+
+    return transformer
