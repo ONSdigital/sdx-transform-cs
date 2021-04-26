e674cb1b2cab40f60b2859ef6ab837c9412a4274
TomHolroyd <tomholroyd96@gmail.com>
2021-04-26 11:55:19 +0100

    dockerfile updated

--- run.py
+++ run.py
@@ -29,7 +29,3 @@ if __name__ == '__main__':
         'workers': 2,
     }
     Server(app, options).run()
-
-# if __name__ == '__main__':
-#     port = 5000
-#     app.run(debug=False, host='0.0.0.0', port=port)
